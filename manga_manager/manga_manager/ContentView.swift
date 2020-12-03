046697fe44005172329caae40c42134d9f10ff11
Developer <developer@Mac-de-Developer.local>
2020-12-03 14:14:50 +0100

    raw_app

---
+++ manga_manager/manga_manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  manga_manager
+//
+//  Created by Developer on 03/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ manga_manager/manga_manager/manga_managerApp.swift
@@ -0,0 +1,17 @@
+//
+//  manga_managerApp.swift
+//  manga_manager
+//
+//  Created by Developer on 03/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct manga_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
